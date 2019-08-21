3ff186c2212387e52c3866875bba31bf5dc6c653
LakshmiMounika123 <lslakshmimounika@gmail.com>
2019-08-21 18:17:35 +0530

    variables

---
+++ variable and assignments/variables.rb
@@ -0,0 +1,30 @@
+
+my_string ='i love my city, pune'
+puts my_string 
+=begin
+  Conversions
+  .to_i, .to_f, .to_s
+=end
+var1=5
+var2='3'
+puts var1+var2.to_i
+
+a="hello"
+a<< "world.
+I love this world.."
+puts a
+
+a = <<END_STR
+This is a string
+And a second line
+END_STR
+puts a
+
+
+# @variable = @variable || "default value"
+
+# def g *args
+#   args
+# end
+
+puts 'It\'s my Ruby'
