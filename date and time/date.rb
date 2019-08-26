955f5ba0d06883ac04fff330790e449827ad4b43
LakshmiMounika123 <lslakshmimounika@gmail.com>
2019-08-26 12:23:21 +0530

    date

---
+++ date and time/date.rb
@@ -0,0 +1,24 @@
+
+t = Time.now
+
+# puts t.strftime("%d/%m/%Y %H:%M:%S")
+
+# puts "Day is " + t.strftime("%A")  #Day
+
+# puts "Month is " + t.strftime("%B") #Month
+
+# puts "Date as M/D/Y " + t.strftime("%D")
+
+# puts "Shortcut day is " + t.strftime("%a")
+
+# puts "Shortcut month is " + t.strftime("%b")
+
+# puts "only date " + t.strftime("%d")
+
+# puts "24 hours clock is " + t.strftime("%H:%M %Z")
+
+require 'date'
+
+puts Date.new(2001,2,3)
+
+puts Date.jd(2451944)
