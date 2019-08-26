66792ae7a055d0fa5d39c086964c0333a096fb18
LakshmiMounika123 <lslakshmimounika@gmail.com>
2019-08-26 23:13:16 +0530

    hashes

--- hashes/hash.rb
+++ hashes/hash.rb
@@ -1,9 +1,11 @@
 
-h = {'dog'=> 'canine', 'cat' => 'feline', 'donkey' => 'asinine', 12 => 'dodecine'}
-puts h.length
-puts h['dog']
-puts h[12]
-puts h
+a = {'dog'=> 'canine', 'cat' => 'feline', 'donkey' => 'asinine', 12 => 'dodecine', 15 => 123456}
+puts a.length
+puts a['dog']
+puts a[12]
+puts a[15]
+a.store("d",42)
+puts a
 
 p = Hash.new
 p[:nickname] = 'IndianGuru'
@@ -12,8 +14,8 @@ p[:lastname] = 'Talim'
  puts p[:lastname]
  puts p
 
- a ={:nickname=>"IndianGuru", :firstname=>"Marathi", :lastname=>"Talim"}
- puts a
+ a1 ={:nickname=>"IndianGuru", :firstname=>"Marathi", :lastname=>"Talim"}
+ puts a1
 
 #  b = {nickname : "IndianGuru", firstname : "Marathi", lastname : "Talim"}
 #  puts b    #error   wrong
@@ -29,3 +31,46 @@ p[:lastname] = 'Talim'
 
 #  e = {1 : 'how are you'}
 #  puts e   #wrong
+
+options = { font_size: 10, font_family: "Arial" }
+puts options[:font_size]
+
+
+#Default value
+grades = Hash.new(0)
+grades = {"Timmy Doe" => 8}
+grades.default = 0
+
+puts grades["Jane Doe"]
+
+
+
+h = Hash.new("Go Fish")
+h["a"] = 100
+h["b"] = 200
+puts h["a"]
+puts h["c"]
+puts h["c"].upcase!
+puts h["e"]
+
+
+h1 = Hash.new{ |hash, key| hash[key] = "Go Fish: #{key}"}
+puts h1["c"]
+puts h1["d"].upcase!
+puts h1["e"]
+puts h1
+print h1.keys
+
+
+#try_convert
+puts Hash.try_convert({1 => 2})
+puts Hash.try_convert("1 => 2")
+
+
+#subset
+h1 = {a:1, b:2}
+h2 = {a:1, b:2, c:3}
+puts h1 < h2    #=> true
+puts h2 < h1    #=> false
+puts h1 < h1    #=> false
+
