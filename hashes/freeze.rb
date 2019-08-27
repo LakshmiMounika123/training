c76629f24877cf4e7e184025dadcd3c6dd5c3ede
LakshmiMounika123 <lslakshmimounika@gmail.com>
2019-08-27 13:20:25 +0530

    freeze and assoc

--- hashes/freeze.rb
+++ hashes/freeze.rb
@@ -10,9 +10,8 @@ print h.assoc("letters")
 
 
 h1 = { "a" => 100, "b" => 200, :c => "c" }
-# h1["a"]        #=> 100
-# h1.compare_by_identity
-# h1.compare_by_identity? #=> true
-# h1["a".100]    #=> nil  # different objects.
-# h1[:c]         #=> "c"  # same symbols are all same.
-
+h1["a"]        #=> 100
+h1.compare_by_identity
+h1.compare_by_identity? #=> true
+h1["a".100]    #=> nil  # different objects.
+h1[:c]         #=> "c"  # same symbols are all same.
